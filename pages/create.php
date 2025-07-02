137e36c54ec129986d36da0ac83a942692d2dd92
Jeremy Stevens <jeremiahstevens@gmail.com>
2025-07-01 22:29:08 -0700

    Add createPasteAdvanced helper and update create page

--- pages/create.php
+++ pages/create.php
@@ -1,7 +1,6 @@
 <?php
 session_start();
-require_once '../includes/db.php';
-require_once '../includes/functions.php';
+require_once __DIR__ . '/../includes/functions.php';
 require_once '../database/init.php';
 $success = false;
 $error = '';
