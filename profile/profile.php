0d3669ce1639c1e266aa7a849bb3b3444dc10256
Jeremy Stevens <jeremiahstevens@gmail.com>
2025-07-06 12:58:47 -0700

    Update profile.php
    Correct include paths

--- profile/profile.php
+++ profile/profile.php
@@ -1,6 +1,5 @@
 <?php
-include(__DIR__ . '/../include/config.php');
-include(__DIR__ . '/../include/session.php');
+include __DIR__ . '/../includes/config.php';
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -11,13 +10,13 @@ include(__DIR__ . '/../include/session.php');
     <link rel="stylesheet" href="/assets/css/style.css">
 </head>
 <body>
-    <?php include(__DIR__ . '/../include/navbar.php'); ?>
+    <?php include __DIR__ . '/../includes/header.php'; ?>
     <main>
         <div class="container mt-5">
             <h1 class="text-white">User Profile</h1>
             <p class="text-light">This page is under construction.</p>
         </div>
     </main>
-    <?php include(__DIR__ . '/../include/footer.php'); ?>
+    <?php include __DIR__ . '/../includes/footer.php'; ?>
 </body>
 </html>
