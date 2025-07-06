e35ae461bfeefee2e01382f6eb5d000ffada9b35
Jeremy Stevens <jeremiahstevens@gmail.com>
2025-07-06 12:16:20 -0700

    Add profile page and menu link

---
+++ profile/profile.php
@@ -0,0 +1,20 @@
+<?php
+include('../include/config.php');
+include('../include/session.php');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>User Profile</title>
+    <link rel="stylesheet" href="/assets/css/style.css">
+</head>
+<body>
+    <?php include('../include/navbar.php'); ?>
+    <div class="container">
+        <h1><i class="fa fa-user"></i> User Profile</h1>
+        <p>This page is under construction.</p>
+    </div>
+</body>
+</html>
